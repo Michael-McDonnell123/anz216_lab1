52845184089393c92dce31f4228971317aa3c71e
Michael McDonnell <Michael.McDonnell067@gmail.com>
2026-09-04 19:32:35 -0500

    Added six attributes to abstract class Ship

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,11 @@
 public abstract class Ship implements Navigable {
+    private String name;
+    private String registrationNumber;
+    private double tonnage;
+    private int crewSize;
+    private double currentSpeed;
+    private String currentPort;
+
+
 
 }
