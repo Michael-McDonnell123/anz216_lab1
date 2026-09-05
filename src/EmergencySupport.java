2ca84344fd233ad0c54e13694a038e1294412dbb
Michael McDonnell <Michael.McDonnell067@gmail.com>
2026-09-04 19:17:32 -0500

    Added two methods to EmergencySupport - deployAid no param and no returns / getEmergencyReadinessReport no param returns a String

--- src/EmergencySupport.java
+++ src/EmergencySupport.java
@@ -1,3 +1,15 @@
 public interface EmergencySupport {
 
+    /**
+     * no param
+     * no return
+     */
+    void deployAid();
+
+    /**
+     *
+     * no param
+     * @return a String
+     */
+    String getEmergencyReadinessReport();
 }
