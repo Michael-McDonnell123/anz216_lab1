4c963b22986248030bf94a971900c1826780c153
Michael McDonnell <Michael.McDonnell067@gmail.com>
2026-09-04 19:21:43 -0500

    Added two methods to PassengerServices - startEntertainment no param and no returns / getPassengerManifest no param returns a String

--- src/PassengerServices.java
+++ src/PassengerServices.java
@@ -1,3 +1,15 @@
 public interface PassengerServices {
 
+    /**
+     * No param
+     * No return
+     * When implemented, the method starts the entertainment service if available
+     */
+    void startEntertainment();
+
+    /**
+     * No param
+     * @return String
+     */
+    String getPassengerManifest();
 }
