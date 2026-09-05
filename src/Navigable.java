17825e6ac3210d75d3385c6ebc48ecdb5f61a67b
Michael McDonnell <Michael.McDonnell067@gmail.com>
2026-09-04 19:11:54 -0500

    Added three methods to Navigable - navigateTo with param String shipsDestination returns nothing / setCurrentSpeed with param double speed returns nothing / getCurrentSpeed no param returns a double

--- src/Navigable.java
+++ src/Navigable.java
@@ -1,3 +1,24 @@
 public interface Navigable {
+    /**
+     *
+     * @param shipsDestination representing the ships destination
+     * does not return anything
+     *
+     */
+    void navigateTo(String shipsDestination);
 
+    /**
+     *
+     * @param speed takes a speed
+     * does not return anything
+     *
+     */
+    void setCurrentSpeed(double speed);
+
+    /**
+     *
+     * no parameters
+     * @return the current speed
+     */
+    double getCurrentSpeed();
 }
